cfb66cd9be0ac8578e0448c7db56bcee01d2ef6f
aegonBalerion <rekhavijay6072@gmail.com>
2025-06-30 16:54:23 +0530

    thoughtfullSphere

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex items-center justify-center h-screen bg-gray-100 text-gray-800 font-sans'>
+      Thoughtful Sphere
+    </div>
+  )
+}
+
+export default App
