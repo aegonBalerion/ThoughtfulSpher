d5e4127c12ad5c8bc89998c8c42dc20a4450c4b4
aegonBalerion <rekhavijay6072@gmail.com>
2025-06-30 22:49:40 +0530

    add signup functionality

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,5 +6,14 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/backend': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react(), tailwindcss(), flowbiteReact()],
 });
+
