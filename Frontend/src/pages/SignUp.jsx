a7b26a57c1ad3e7d763dbb5fa18c3d7ea39a0d02
aegonBalerion <rekhavijay6072@gmail.com>
2025-07-01 13:11:03 +0530

    Google Auth

--- Frontend/src/pages/SignUp.jsx
+++ Frontend/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
+import OAuth from '../components/OAuth';
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
@@ -84,7 +84,7 @@ export default function SignUp() {
               />
             </div>
             <Button
-              // gradientduotone='purpleToPink'
+              // gradientDuoTone='purpleToPink'
               type='submit'
               disabled={loading}
             >
@@ -97,6 +97,7 @@ export default function SignUp() {
                 'Sign Up'
               )}
             </Button>
+            <OAuth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an account?</span>
